2d12e056e56d6fb3afb564439a8222fc2026627b
MatthewColvin <mcolvin32.7@gmail.com>
2025-03-23 21:43:09 -0500

    pull flags out of hw rev1 as they are no longer needed.

--- Platformio/lib/esp32HalImpl/HardwareRev1.cpp
+++ Platformio/lib/esp32HalImpl/HardwareRev1.cpp
@@ -3,15 +3,13 @@
 #include "omoteconfig.h"
 
 void HardwareRev1::init() {
-#if not defined(OMOTE_HARDWARE_REV5)
+  HardwareRevX::init();
   mKeys = std::make_shared<Keys>();
   mBattery = std::make_shared<Battery>(ADC_BAT, CRG_STAT);
-#endif
 }
 
 void HardwareRev1::initIO() {
   HardwareRevX::initIO();
-#if not defined(OMOTE_HARDWARE_REV5)
   pinMode(SW_1, OUTPUT);
   pinMode(SW_2, OUTPUT);
   pinMode(SW_3, OUTPUT);
@@ -22,30 +20,22 @@ void HardwareRev1::initIO() {
   pinMode(SW_C, INPUT);
   pinMode(SW_D, INPUT);
   pinMode(SW_E, INPUT);
-#endif
 
-#if not defined(OMOTE_HARDWARE_REV5)
   pinMode(ADC_BAT, INPUT);
-#endif
 
-#if not defined(OMOTE_HARDWARE_REV5)
   gpio_hold_dis((gpio_num_t)SW_1);
   gpio_hold_dis((gpio_num_t)SW_2);
   gpio_hold_dis((gpio_num_t)SW_3);
   gpio_hold_dis((gpio_num_t)SW_4);
   gpio_hold_dis((gpio_num_t)SW_5);
-#endif
 }
 
 void HardwareRev1::sleepDisplayPins() {
-#if not defined(OMOTE_HARDWARE_REV5)
   digitalWrite(LCD_MOSI, LOW);
   digitalWrite(LCD_SCK, LOW);
-#endif
 }
 
 void HardwareRev1::configPinsForSleepInterrupts() {
-#if not defined(OMOTE_HARDWARE_REV5)
   pinMode(SW_1, OUTPUT);
   pinMode(SW_2, OUTPUT);
   pinMode(SW_3, OUTPUT);
@@ -61,5 +51,4 @@ void HardwareRev1::configPinsForSleepInterrupts() {
   gpio_hold_en((gpio_num_t)SW_3);
   gpio_hold_en((gpio_num_t)SW_4);
   gpio_hold_en((gpio_num_t)SW_5);
-#endif
 };

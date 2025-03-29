060cad9b622623010cbf491094ebcfdc2f8aeb0e
MatthewColvin <mcolvin32.7@gmail.com>
2025-03-29 12:05:33 -0500

    invert I2c display to correct color

--- Platformio/lib/esp32HalImpl/display/display.cpp
+++ Platformio/lib/esp32HalImpl/display/display.cpp
@@ -47,7 +47,7 @@ LGFX::LGFX(void) {
     cfg.dummy_read_pixel =     8;  
     cfg.dummy_read_bits  =     1;  
     cfg.readable         =  true;  
-    cfg.invert           = true;  
+    cfg.invert           = false;  
     cfg.rgb_order        = false;  
     cfg.dlen_16bit       = false;  
     cfg.bus_shared       =  true; 

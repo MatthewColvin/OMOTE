321d6caeeb9e9856f1942a550f172daabb3e6ebe
MatthewColvin <mcolvin32.7@gmail.com>
2025-10-12 16:06:40 -0500

    Fix Bad define in new IMU class

--- Platformio/lib/esp32HalImpl/imu/LIS3DH_IMU.cpp
+++ Platformio/lib/esp32HalImpl/imu/LIS3DH_IMU.cpp
@@ -115,7 +115,7 @@ void LIS3DH_IMU::configIMUInterrupts(bool aWakeupByIMUEnabled) {
 }
 
 void LIS3DH_IMU::configIMUInterruptPolarity() {
-#if defined(OMOTE_HARDWARE_) || defined(OMOTE_HARDWARE_REV6)
+#if defined(OMOTE_HARDWARE_REV5)
   mIMU.writeRegister(LIS3DH_CTRL_REG6, 0x02); // For active-low interrupt
 #else
   mIMU.writeRegister(LIS3DH_CTRL_REG6, 0x00); // For active-high interrupt

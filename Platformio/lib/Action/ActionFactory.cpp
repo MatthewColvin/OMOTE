db4c046e56e943c4dfa5dbc203d82db6052474dc
MatthewColvin <mcolvin32.7@gmail.com>
2025-11-13 21:14:33 -0600

    Tweak create Action API that is meant to lookup action by name to use common parsing apis

--- Platformio/lib/Action/ActionFactory.cpp
+++ Platformio/lib/Action/ActionFactory.cpp
@@ -42,17 +42,10 @@ std::unique_ptr<IAction> ActionFactory::createAction(const rapidjson::Value &aPr
 std::unique_ptr<IAction> ActionFactory::createAction(const std::string &aActionName) {
   for (auto const &dir_entry : std::filesystem::directory_iterator{ActionsDirectory}) {
     if (dir_entry.is_regular_file()) {
-      std::string fullPath(FS_PATH);
-      fullPath += dir_entry.path().string();
-      std::ifstream file(fullPath, std::ios::in);
-      if (file) {
-        rapidjson::Document actionDoc;
-        std::stringstream buffer;
-        buffer << file.rdbuf();
-        file.close();
-        std::string actionJsonStr(buffer.str());
-        actionDoc.Parse(actionJsonStr.c_str());
-        if (actionDoc.HasMember("name") && actionDoc.IsString()) {
+      rapidjson::Document actionDoc = OMOTE::JSON::GetDocument(dir_entry.path());
+      if (actionDoc.HasMember("name") && actionDoc["name"].IsString()) {
+        std::string actionNameInFile = actionDoc["name"].GetString();
+        if (actionNameInFile == aActionName) {
           return createAction(actionDoc);
         }
       }
